de584b7545401d49dadf53cea526f396caee8730
Michail Tsoma <dagobertusdagobertus@gmail.com>
2020-04-11 18:38:22 +0300

    email

--- index.js
+++ index.js
@@ -2,14 +2,26 @@ const express = require('express')
 const path = require('path')
 const app = express() 
 
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html') )
 })
 
+app.post('/', (req, res) => {
+  console.log(req.body)
+
+  res.redirect('/')
+
+})
 app.get('/portfolio', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'portfolio.html'))
 })
-app.use(express.static(path.join(__dirname, 'public')))
+
+
+
 
 const PORT = process.env.PORT || 3000
 
--- public/main.js
+++ public/main.js
@@ -1,6 +1,5 @@
 M.AutoInit();
 
-
 const activeElement = document.querySelectorAll('#service'); 
 const scrollsLine = document.querySelectorAll('.scrollsLine');  
 
@@ -21,8 +20,62 @@ activeElement.forEach((elem, index) => {
     scrollsLine.forEach(elem => elem.style.backgroundColor = '#fff')
 
     document.querySelector(id).style.display = 'block'
+    document.querySelector(id).classList.add('animated', 'fadeIn')
     elem.parentElement.nextElementSibling.style.display = 'block'
     scrollsLine[index].style.backgroundColor = 'brown' 
   } )
 })
 
+
+//carousel
+
+let carousel = document.querySelectorAll('.inner-carousel a'); 
+let indicators = document.querySelectorAll('.carousel-indicator') 
+
+carousel.forEach(el => {
+  el.addEventListener('click', () => {
+    setTimeout(() => {
+      carousel.forEach(element => {
+        if (element.classList.contains('active')){
+         
+          let id  = element.dataset.id 
+          indicators.forEach(e => {
+            e.classList.remove('fon')
+          })
+          indicators.forEach(e => {
+            if(e.id === id){
+              e.classList.add('fon')
+            }
+          }) 
+        }
+      })
+    }, 200 )
+ 
+
+  })
+})
+
+const scrolling = (upSelector) => {
+  const upElem = document.querySelector(upSelector);
+
+  window.addEventListener('scroll', () => {
+      if (document.documentElement.scrollTop > 1650) {
+          upElem.classList.add('animated', 'fadeIn');
+          upElem.classList.remove('fadeOut');
+      } else {
+          upElem.classList.add('fadeOut');
+          upElem.classList.remove('fadeIn');
+      }
+  });
+}
+scrolling('.pageup')
+
+let t;
+function up() {
+	let top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
+	if(top > 0) {
+		window.scrollBy(0,-100);
+		t = setTimeout('up()',20);
+	} else clearTimeout(t);
+	return false;
+}
